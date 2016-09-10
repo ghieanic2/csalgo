c2c5481ca3f7ab6a97ae83fa65b58537141c504c
MA. ANJELICA LOUISE ANICETO <MOANICETO@fit.edu.ph>
2016-09-10 21:03:46 +0800

    Prints string as an array

--- LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
+++ LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
@@ -16,6 +16,12 @@ public class LexicographicGenerator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String charset = "ABC";
+        
+        for(int i=0; i<charset.length(); i++){
+            System.out.println(charset.charAt(i));
+        }
     }
     
 }
